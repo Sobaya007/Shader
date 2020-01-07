3a0bd2015eddb574361fec2994d2c2557b1079d3
sobaya <sobaya007@gmail.com>
2020-01-07 15:50:24 +0900

    WIP

--- project/hanger.d
+++ project/hanger.d
@@ -3,11 +3,11 @@ import sbylib;
 import frag;
 import rot;
 
-// mixin(Register!entryPoint);
+mixin(Register!entryPoint);
 void entryPoint(Project proj, ModuleContext context, Window window) {
     auto tex = new FileTexture("resource/spheremap.jpg");
     context.pushResource(tex);
-    auto bolt = Basami.create(window, tex);
+    auto bolt = Hanger.create(window, tex);
     context.pushResource(bolt);
 
     Rot q;
@@ -20,7 +20,7 @@ void entryPoint(Project proj, ModuleContext context, Window window) {
                 rot = q.toMatrix4;
                 time = t;
             }
-            t += 0.2;
+            t += 0.02;
         });
 
         when(KeyButton.KeyR.pressed.on(window)).then({
@@ -114,108 +114,97 @@ vec3 scale(vec3 p, vec3 s) {
   return p / s;
 }
 
-float poll(vec3 p) {
-  p = scale(p, vec3(1.2, 1.5, 1));
-  p = translate(p, vec3(-0.5,0.3,0));
-  vec2 d = abs(vec2(length(p.xy), p.z)) - 1;
-  float result = min(max(d.x, d.y), 0) + length(max(d, 0));
-  result = max(result, -p.y);
-  return result;
+float distHook(vec3 p) {
+  p = rotate(p, vec3(0,1,0), uni.time * uni.time * 3);
+  p = scale(p, vec3(0.8, 1,1));
+  p = translate(p, vec3(0,1,0));
+  float sub = length(translate(p, vec3(-0.7,-0.7,0))) - 0.8;
+  vec2 q = vec2(length(p.xy) - 1, p.z);
+  return max(length(q) - 0.1, -sub);
 }
 
-float poll2(vec3 p) {
-  p = translate(p, vec3(-4.2,1.5,0));
-  p = rotate(p, vec3(0,0,1), 20);
-  p = scale(p, vec3(2, 1.5, 15));
-  vec2 d = abs(vec2(length(p.xy), p.z)) - 1;
-  float result = min(max(d.x, d.y), 0) + length(max(d, 0));
-  return result;
+float distPoll(vec3 p) {
+  p = translate(p, vec3(0,-1,0));
+  p = scale(p, vec3(1,5,1));
+  vec2 d = abs(vec2(length(p.xz), p.y)) - 0.15;
+  return min(max(d.x, d.y), 0) + length(max(d,0));
 }
 
-float body(vec3 p) {
-  float sub3 = p.y + 0.5;
-  const float freq = 0.1;
-  float triWave = (1 - abs(mod(p.x*10,2)-1)) * 0.2;
-  vec3 n = normalize(vec3(-0.3, 1, 0));
-  float sub2 = dot(p - vec3(-4.2, 1.2, 0) - n * triWave, normalize(-n));
-  p = translate(p, vec3(10 / 18. + 0.3, 0,0));
-  float result = length(max(abs(p) - vec3(10,1,1), 0.0f));
-  float sub = dot(p - vec3(0,0.5,0), normalize(vec3(0.15,1,0)));
-  result = max(result, sub);
-  result = max(result, -sub2);
-  result = max(result, -sub3);
-  return result;
+float distMetalBall(vec3 p) {
+  p = rotate(p, vec3(0,1,0), uni.time * uni.time * 3);
+  p = translate(p, vec3(-0.80,0.9,0));
+  return length(p) - 0.2;
 }
 
-float ringOnly(vec3 p) {
-  float rate = 1 + (sin(uni.time /10) + 1) * 0.05;
-  p = scale(p, vec3(rate,1,rate));
-  vec2 q = vec2(length(p.xy) - 4, p.z);
-  float result = length(q) - 0.3;
-  return result;
-}
-
-float ball(vec3 p) {
-  p = scale(p, vec3(0.5,0.5,10) );
-//  p = translate(p, vec3(0,-7,0));
-  return length(p) - 1;
-}
-
-float side(vec3 p) {
-  float sub3 = length(p - vec3(0,-4,0)) - 1.2;
-  p = scale(p, vec3(0.65, 1,1));
+float distMetalUnder(vec3 p) {
   p.x = abs(p.x);
-  p = translate(p, vec3(1.2,0,0));
-  p = rotate(p, vec3(0,0,1), 74 + 8 * (sin(uni.time / 10) + 1));
-  //p = rotate(p, vec3(0,0,1), 76);
-  p = translate(p, vec3(0.5, -1.4, 0));
-  float sub2 = poll2(p);
-  float result = body(p);
-  result = smin(result, poll(p), 4);
-  result = max(result, abs(p.z) - 1);
-  result -= 0.5;
-  result = max(result, -sub2);
-  result = max(result, -sub3);
+  p = translate(p, vec3(4,-5,0));
+  p = scale(p, vec3(1,10,1));
+  vec2 d = abs(vec2(length(p.xz), p.y)) - 0.05;
+  return min(max(d.x, d.y), 0) + length(max(d,0));
+}
+
+float distMetal(vec3 p) {
+  float result = smin(distHook(p), distPoll(p), 17);
+  result = smin(result, distMetalBall(p), 17);
+  result = min(result, distMetalUnder(p));
   return result;
 }
 
-float ring(vec3 p) {
-  float sub3 = length(p - vec3(0,-4,0)) - 1.2;
-  float r = ringOnly(p);
-  p = scale(p, vec3(0.65, 1,1));
+float distWood1(vec3 p) {
+  p = scale(p, vec3(0.1+abs(p.y) * 0.5,1,1));
+  p = translate(p, vec3(0,-1.5,0));
+  return length(max(abs(p) - vec3(1.0,0.6,0.2), 0));
+}
+
+float distWood2(vec3 p) {
   p.x = abs(p.x);
-  p = translate(p, vec3(1.2,0,0));
-  p = rotate(p, vec3(0,0,1), 74 + 8 * (sin(uni.time / 10) + 1));
-  //p = rotate(p, vec3(0,0,1), 76);
-  p = translate(p, vec3(0.5, -1.4, 0));
-  float sub2 = poll2(p);
-  float result = r;
-  result = max(result, -sub2);
-  result = max(result, -sub3);
-  return result;
+  p = translate(p, vec3(0,-1.5,0));
+  p = rotate(p, vec3(0,0,1) , -35);
+  p = scale(p, vec3(1, p.x * p.x * 0.07 +2, p.x * p.x * p.x * 0.03 +1));
+  vec3 s = vec3(0);
+  vec3 e = vec3(5, 0, 0);
+  vec3 ps = p - s;
+  vec3 es = e - s;
+  float h = clamp(dot(ps, es) / dot(es, es), 0, 1);
+  float d = length(ps - es * h) - 0.2;
+  float sub = p.y;
+  return max(d, -sub);
+}
+
+float distWood(vec3 p) {
+  return smin(distWood1(p), distWood2(p), 17);
+}
+
+
+float distUnder(vec3 p) {
+  p = translate(p, vec3(0,-5.5,0));
+  p = scale(p, vec3(20,1,1));
+  vec2 d = abs(vec2(length(p.yz), p.x)) - 0.2;
+  return min(max(d.x, d.y), 0) + length(max(d,0));
 }
 
 float dist(vec3 p) {
-//  float py = p.y+8;
-//  p.x+=(1-1/pow(py*py+1,0.4))*pow(py,1.8)*0.05*sin(uni.time/2);
-//  p.z+=(1-1/pow(py*py+1,0.4))*pow(py,1.8)*0.05*sin(uni.time/1.34235324);
-//  float result = side(p);
-//  result = min(result, ring(p));
-//  return result-0.5-0.1*sin(uni.time*0.2)*pow((sin(p.x*4)+sin(p.y*4)+sin(p.z*4)),2);
-//  return result;
-  float result = side(p);
-  result = min(result, ring(p));
-//  return result-0.2-0.08*(sin(uni.time/3+p.x*p.x*0.4)+sin(20 * uni.time/3+p.y*p.y*0.10)+sin(uni.time/3+p.z*p.z*0.3));
+  p = translate(p, vec3(0,3,0));
+  float result = min(distMetal(p), distWood(p));
+  result = min(result, distUnder(p));
   return result;
+  //return result-0.2-0.04*(sin(uni.time/3+p.x*p.x*0.3)+sin(20 * uni.time/3+p.y*p.y*0.20)+sin(uni.time/3+p.z*p.z*0.3));
 }
 
 int getNearestIndex(vec3 p) {
-  float minDist = side(p);
+  p = translate(p, vec3(0,3,0));
+  float dist = distMetal(p);
   int idx = 0;
-  float d = ring(p);
-  if (d < minDist) {
-    minDist = d;
+  float d = distWood(p);
+  if (d < dist) {
+    dist = d;
     idx = 1;
+  }
+  d = distUnder(p);
+  if (d < dist) {
+    dist = d;
+    idx = 2;
   }
   return idx;
 }
@@ -253,14 +242,12 @@ vec2 cubeMap(vec3 current, vec3 vec) {
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.y, tmp.z);
-    minT = t;
   }
 
-  t = (-size - current.x) / vec.x;
+  t = -t;
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.y, tmp.z);
-    minT = t;
   }
 
   //===============y
@@ -268,14 +255,12 @@ vec2 cubeMap(vec3 current, vec3 vec) {
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.z, tmp.x);
-    minT = t;
   }
 
-  t = (-size - current.y) / vec.y;
+  t = -t;
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.z, tmp.x);
-    minT = t;
   }
 
   //==============z
@@ -283,21 +268,20 @@ vec2 cubeMap(vec3 current, vec3 vec) {
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.x, tmp.y);
-    minT = t;
   }
 
-  t = (-size - current.z) / vec.z;
+  t = -t;
   if (0 < t && t < minT) {
     tmp = current + t * vec;
     result = vec2(tmp.x, tmp.y);
-    minT = t;
   }
   return result  / (2 * size) + 1;
 }
 
 
 void main() {
-  vec3 eye = vec3(0,0,-20);
+
+  vec3 eye = vec3(0,0,-15);
   eye = (uni.rot * vec4(eye,0)).xyz;
   vec3 ray = vec3(tc, sqrt(2) * tan(fov));
   const float angle = 1.5;
@@ -315,18 +299,20 @@ void main() {
     vec3 refVec = reflect(-lightVec, n);
     float diffuse = max(0,dot(eyeVec, n));
     float spec = max(0,dot(eyeVec, refVec));
-    vec4 reflectColor = texture(mTexture, cubeMap(current, reflect(-eyeVec, n)));
     switch (getNearestIndex(current)) {
       case 0:
-        float refRate = 0.05;
-        FragColor = vec4(0.9,0.7,0.7,1) * diffuse;
-        FragColor = FragColor * (1-refRate) + reflectColor * refRate;
-        FragColor += pow(spec, 5) * 0.2;
-        break;
-      case 1:
-        FragColor = reflectColor * 0.2 + vec4(0.5, 0.5 ,0.5, 1) * 0.8;
+        vec4 reflectColor = texture(mTexture, cubeMap(current, refVec));
+        FragColor = reflectColor * 0.2 + vec4(0.5,0.5,0.5,1) * 0.8;
         FragColor *= diffuse;
         FragColor += pow(spec, 5);
+        break;
+      case 1:
+        vec3 dir = vec3(0.01,0.2,0.5);
+        float t = noise(current * noise(current * 100) * 3000 * dir);
+        vec4 lightColor = vec4(213,136,76,255) / 255;
+        vec4 darkColor = vec4(vec3(lightColor.rgb / 4), 1);
+        FragColor = mix(lightColor, darkColor, t) * diffuse;
+        FragColor += darkColor * pow(spec, 30) * (1-t);
         break;
       case 2:
         FragColor = vec4(0,0,0,1) * diffuse;
@@ -348,4 +334,4 @@ align(16):
     mat4 rot;
 }
 
-alias Basami = FragmentCanvas!(fragmentSource, Uniform, 1);
+alias Hanger = FragmentCanvas!(fragmentSource, Uniform, 1);
